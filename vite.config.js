f6206fab0bd9deb8e1dbb67aad7afdc089c06b25
Julian Herrera <julian.herrera@live.com>
2024-04-02 11:12:25 -0500

    update repo-base

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import postcssNesting from "postcss-nesting";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/<REPO>/",
+  base: "good-soup",
   css: {
     postcss: {
       plugins: [postcssNesting],
