475b5085417547221243de7e11ef34f844cece26
Julian Herrera <julian.herrera@live.com>
2024-04-02 23:00:06 -0500

    remove console.logs

--- src/screens/Recipe.jsx
+++ src/screens/Recipe.jsx
@@ -5,7 +5,6 @@ import { secondsToDuration, calculateAdjustedDuration } from "../utils";
 import { useState, useRef, useEffect } from "react";
 
 export default ({ developmentRecipe }) => {
-  console.log(developmentRecipe);
   const formRef = useRef();
 
   // Track the Current Step
@@ -66,7 +65,6 @@ export default ({ developmentRecipe }) => {
       return acc;
     }, {});
 
-    console.log(adjustedDurations);
     setAdjustedDurations(adjustedDurations);
   }, [developmentRecipe.steps, mixState]);
 
