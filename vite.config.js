a57f9b2f8e7f01352498e70e5dfda4c14d86c6a3
Julian Herrera <julian.herrera@live.com>
2024-04-02 23:46:32 -0500

    switch out for PWA-vite-plugin

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import postcssNesting from "postcss-nesting";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -10,5 +11,30 @@ export default defineConfig({
       plugins: [postcssNesting],
     },
   },
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      // generates 'manifest.webmanifest' file on build
+      manifest: {
+        // caches the assets/icons mentioned (assets/* includes all the assets present in your src/ directory)
+        includeAssets: ["favicon.ico", "apple-touch-icon.png", "assets/*"],
+        name: "Good Soup",
+        short_name: "Good Soup",
+        start_url: "/good-soup/",
+        background_color: "#ffffff",
+        theme_color: "#000000",
+        icons: [
+          {
+            src: "icon.png",
+            sizes: "700x700",
+            type: "image/png",
+          },
+        ],
+      },
+      workbox: {
+        // defining cached files formats
+        globPatterns: ["**/*.{js,css,html,ico,png,svg,webmanifest}"],
+      },
+    }),
+  ],
 });
