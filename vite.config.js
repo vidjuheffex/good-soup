c6dcf57335ded701888ce3f52d1d8bed7a39f5ff
Julian Herrera <julian.herrera@live.com>
2024-04-03 14:06:28 -0500

    pwa stuff

--- vite.config.js
+++ vite.config.js
@@ -26,9 +26,25 @@ export default defineConfig({
         theme_color: "#000000",
         icons: [
           {
-            src: "icon.png",
-            sizes: "700x700",
+            src: "pwa-64x64.png",
+            sizes: "64x64",
             type: "image/png",
+          },
+          {
+            src: "pwa-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "pwa-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+          {
+            src: "maskable-icon-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "maskable",
           },
         ],
       },
