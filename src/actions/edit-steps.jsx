0903b3302fa266cdf8e4c88b7547cdd144ed1bee
Julian Herrera <julian.herrera@live.com>
2024-04-19 13:46:20 -0500

    fix: edit steps not getting time-converted

--- src/actions/edit-steps.jsx
+++ src/actions/edit-steps.jsx
@@ -1,4 +1,5 @@
 import { getOneFromStore, open, putObjectInStore } from "../db";
+import { durationToSeconds } from "../utils";
 
 export default async (developmentRecipeId, steps) => {
   const db = await open();
@@ -9,10 +10,21 @@ export default async (developmentRecipeId, steps) => {
     "by_id",
     developmentRecipeId,
   );
+
+  const parsedSteps = JSON.parse(steps).map((step) => ({
+    ...step,
+    duration: durationToSeconds(step.duration),
+    agitationIntervals: durationToSeconds(step.agitationIntervals),
+    temp: parseInt(step.temp, 10),
+    agitationTime: durationToSeconds(step.agitationTime),
+    initialAgitation: durationToSeconds(step.initialAgitation),
+  }));
+
   const newDevelopmentRecipe = {
     ...developmentRecipe,
-    steps: JSON.parse(steps),
+    steps: parsedSteps,
   };
+
   await putObjectInStore(db, "development-recipes", newDevelopmentRecipe);
   return null;
 };
